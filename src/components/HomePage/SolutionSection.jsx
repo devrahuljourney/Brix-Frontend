109cdd962f32e73954f18da7e434914fdf565a8d
rahulkumarv269 <rahulkumarv269@gmail.com>
2024-03-03 05:20:37 +0530

    Solution section completed

--- src/components/HomePage/SolutionSection.jsx
+++ src/components/HomePage/SolutionSection.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import HighLightText from '../common/HighLightText'
-import box from '../../assets/box.png'
-import SolutionCard from '../common/SolutionCard'
-import '../../style/SolutionSection.css'
+// SolutionSection.js
+import React from 'react';
+import HighLightText from '../common/HighLightText';
+import box from '../../assets/box.png';
+import SolutionCard from '../common/SolutionCard';
+import '../../style/SolutionSection.css';
+
 export default function SolutionSection() {
   return (
-    <div className='solution__container' >
-        <HighLightText text1='Solutions we deliver' text2='at The Brix' img={box} color='#D225E1' />
-        <SolutionCard/>
+    <div className='solution__container'>
+      <HighLightText text1='Solutions we deliver' text2='at The Brix' img={box} color='#D225E1' />
+      <div className='scrollable__container'>
+        <SolutionCard />
+      </div>
     </div>
-  )
+  );
 }
--- src/components/common/SolutionCard.jsx
+++ src/components/common/SolutionCard.jsx
@@ -8,20 +8,21 @@ export default function SolutionCard() {
   return (
     <div className='card__container'>
       {solutionData.map((data, index) => (
-        <div  onMouseEnter={() => setHoveredIndex(index)}
-            onMouseLeave={() => setHoveredIndex(null)}
+        <div 
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
           className='button' 
           key={index} 
           style={{ 
-            boxShadow: hoveredIndex === index && `0 0 10px ${data.color}, 0 0 40px ${data.color}, inset 0 0 10px ${data.color}, inset 0 0 40px ${data.color} , `,
-            background: hoveredIndex === index && ` ${data.color} `, 
+            boxShadow: hoveredIndex === index && `0 0 30px ${data.color},
+  0 0 40px ${data.color}`,
+            background: hoveredIndex === index && data.color,
             border: `1px solid ${data.color}`
           }}
         >
           <a
-            className='link'
+            className='link-btn'
             href={data.link}
-            
           >
             {data.name}
           </a>
